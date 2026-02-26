aa0034c447ae93cc6c84f1ac1bdcfda9aad43fb8
Manthan Harale <manthanji55@gmail.com>
2026-02-26 08:47:04 -0800

    update in telwindcss in section1

--- app/coponents/section1.jsx
+++ app/coponents/section1.jsx
@@ -7,10 +7,10 @@ import React from 'react'
 
 const Section1 = () => {
   return (
-    <div className='h-screen w-86 flex flex-col items-center justify-between '>
+    <div className='h-full w-86 flex flex-col items-center justify-between '>
 
       {/* works */}
-      <div className='bg-[#101010] overflow-hidden mb-3 sm:mb-0 relative border border-[#141414] w-full h-[231px] rounded-lg'>
+      <div className='bg-[#101010] overflow-hidden mb-3 sm:mb-0 relative border border-[#141414] w-full h-57.75 rounded-lg'>
 
         <div className="absolute  left-0 top-0 bottom-0 w-full  bg-gradient-to-t from-[#101010] via-[#101010]/30  to-transparent z-10 pointer-events-none"></div>
         {/* heding section */}
@@ -25,9 +25,9 @@ const Section1 = () => {
         </div>
 
         {/* project img section */}
-        <div className='w-full h-[80px] relative'>
+        <div className='w-full h-20 relative'>
           
-          <div className='w-full h-[83px] px-2 mt-8 flex items-center justify-between '>
+          <div className='w-full h-0.75 px-2 mt-8 flex items-center justify-between '>
           <Image
           width={90}
           height={70}
@@ -51,7 +51,7 @@ const Section1 = () => {
           />
         </div>
         <Link href="/Projects">
-        <button className='h-[40px] cursor-pointer w-[200px] z-10 rounded-md bg-[#773dff] text-white absolute top-15 left-[70px] '>View Works</button>
+        <button className='h-10 cursor-pointer w-50 z-10 rounded-md bg-[#773dff] text-white absolute top-15 left-17.5 '>View Works</button>
         </Link>
         </div>
         
@@ -59,7 +59,7 @@ const Section1 = () => {
       </div>
       
       {/* about */}
-      <div className=' relative bg-[#101010] border mb-3 sm:mb-0 border-[#141414] w-full h-[231px] rounded-lg flex flex-col items-center justify-center '>
+      <div className=' relative bg-[#101010] border mb-3 sm:mb-0 border-[#141414] w-full h-57.75 rounded-lg flex flex-col items-center justify-center '>
 
         <div className="absolute  left-0 top-0 bottom-0 w-full  bg-gradient-to-t from-[#101010] via-[#101010]/30  to-transparent z-10 pointer-events-none"></div>
         
@@ -75,7 +75,7 @@ const Section1 = () => {
               </h2>
         </div>
 
-          <div className='w-full h-[83px] px-2 mt-8 flex items-center justify-between '>
+          <div className='w-full h-20.75 px-2 mt-8 flex items-center justify-between '>
           <Image
           width={90}
           height={70}
@@ -104,7 +104,7 @@ const Section1 = () => {
       </div>
 
       {/* resume section */}
-       <div className='bg-[#101010] overflow-hidden mb-3 sm:mb-0 relative border border-[#141414] w-full h-[231px] rounded-lg'>
+       <div className='bg-[#101010] overflow-hidden mb-3 sm:mb-0 relative border border-[#141414] w-full h-57.75 rounded-lg'>
 
         <div className="absolute  left-0 top-0 bottom-0 w-full  bg-gradient-to-t from-[#101010] via-[#101010]/30  to-transparent z-10 pointer-events-none"></div>
         {/* heding section */}
@@ -119,9 +119,9 @@ const Section1 = () => {
         </div>
 
         {/* resume img section */}
-        <div className='w-full h-[80px] relative top-4.5'>
+        <div className='w-full h-20 relative top-4.5'>
           
-          <div className='w-full h-[83px] px-2 mt-8 flex items-center justify-between '>
+          <div className='w-full h-20.75 px-2 mt-8 flex items-center justify-between '>
           <Image
           width={90}
           height={70}
@@ -146,7 +146,7 @@ const Section1 = () => {
         </div>
 
         <a href="https://docs.google.com/document/d/1aObb2AhOEqwt7rFXbx1zu6yhaJU1xImEQaqRcmnR_OE/edit?usp=sharing">
-        <button className='h-[40px] w-[200px] z-10 rounded-md bg-[#773dff] text-white absolute top-15 left-[70px] flex items-center justify-center '>
+        <button className='h-10 w-50 z-10 rounded-md bg-[#773dff] text-white absolute top-15 left-17.5 flex items-center justify-center '>
           <Download/> <span className='ml-1'>Resume</span>
           </button>
           </a>
