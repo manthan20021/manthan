f5fc0960210c5f76a4af3b658ad554bc5490b6ba
Manthan Harale <manthanji55@gmail.com>
2026-02-26 08:22:39 -0800

    w and h fix

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import Setion3 from "./coponents/setion3";
 export default function Home() {
   return (
     <div className="sm:w-screen sm:h-full h-full w-full  py-6 px-10 bg-black flex items-center justify-center">
-    <div className="sm:w-[1400px] sm:h-[720px] w-full h-full  flex items-center justify-between md:flex-row flex-col ">
+    <div className="sm:w-350 sm:h-180 w-full h-full  flex items-center justify-between md:flex-row flex-col ">
       <Section1/>
       <Section2/>
       <Setion3/>
