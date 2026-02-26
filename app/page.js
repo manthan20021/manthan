e8d4806c6a4a5e2fd74cdd4ee072d2030dd68cba
Manthan Harale <manthanji55@gmail.com>
2026-02-26 04:15:03 -0800

    h-full

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Setion3 from "./coponents/setion3";
 
 export default function Home() {
   return (
-    <div className="sm:w-screen sm:h-screen h-full w-full  py-6 px-10 bg-black flex items-center justify-center">
+    <div className="sm:w-screen sm:h-full h-full w-full  py-6 px-10 bg-black flex items-center justify-center">
     <div className="sm:w-[1400px] sm:h-[720px] w-full h-full  flex items-center justify-between md:flex-row flex-col ">
       <Section1/>
       <Section2/>
